f6a862bae50aab666291548581a0633d74a77ab2
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-29 20:42:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |cart| 
-        resp.write "#{cart}\n"
+      if @@cart.empty?
       end 
     else
       resp.write "Path Not Found"
