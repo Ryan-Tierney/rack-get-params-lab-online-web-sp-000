1d77cde75b5951a780d6b830326b74e105e04f5b
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-29 20:47:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,8 @@ class Application
       search_term = req.params["item"]
       if @@items.include?(search_term) 
         @@cart << search_term 
+        resp.write "added #{search_term}"
+      end 
     else
         resp.write "Path Not Found"
   end 
